3746213eb7d4d37a13e374af7435c3029024a065
BellaMpho <your_email@example.com>
2025-04-18 17:38:57 +0200

    git commit -m "Add all vehicle management files and implement booking system, payments, and employee management features."

--- src/main/java/com/example/vehiclemanagement/DatabaseConnection.java
+++ src/main/java/com/example/vehiclemanagement/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/vehiclemanagement";
+    private static final String URL = "jdbc:mysql://localhost:3306/vehiclerentaldb";
     private static final String USER = "root";
-    private static final String PASSWORD = "901017297";
+    private static final String PASSWORD = "666666";
 
     private static Connection connection = null;
 
